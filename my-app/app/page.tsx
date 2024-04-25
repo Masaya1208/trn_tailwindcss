101a294b4e4e4a5c661fa2e14e88c5eef73066f8
Masaya1208 <kato.envsys@gmail.com>
2024-04-25 11:22:46 +0900

    mainの体裁を整えました

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
             </nav>  
           </div>
           {/*メイン*/}
-          <div className="text-center">
+          <div className="w-11/12 md:w-10/12 lg:w-1/2 mx-auto">
             <div>
               <div className="bg-blue-100 inline-block py-1 px-4 rounded-xl mb-6">
                 <p className="text-blue-500 font-bold">
@@ -34,35 +34,39 @@ export default function Home() {
             </div>
 
             <section className="mt-10 mb-32 blog">
-              <h1>
-                1 min read</h1>
-              <div className="mt-8">
-                <div className="paragraph-block">
-                  <p className="mb-6 leading-relaxed">
-                  TailwindTemplates V2 is finally here! Our focus for TailwindTemplates v2 is to bring open-source design systems to Tailwindcss. Each design system has a detailed guideline to provide consistency and coherence within a user interface.</p>
-                </div>
-                <div className="header-brock">
-                  <h3 className="mb-6 font-bold">
-                  What to expect?</h3>
-                </div>
-                <div className="paragraph-block">
-                <p className="mb-6 leading-relaxed">
-                  Over the coming months, we will expand the design systems that are currently available at launch. In addition to that, we are planning to create several tailwind configurations to easily get started with the proper colors and variants that belong to each design system. </p>
-                </div>
-                <div className="header-brock">
-                  <h3 className="mb-6 font-bold">
-                  Not only templates</h3>
-                </div>
-                <div className="paragraph-brock">
-                  <p className="mb-6 leagind-relaxed">
-                  We are planning to bring more tailwind and web development-related content such as videos, blog posts, tutorials and news. </p>
+              <div className="flex justfy-between items-center">
+                <div className="flex item-center">
+                  <p className="text-ui-secoudary">
+                    1 min read</p>
                 </div>
               </div>
+              <div className="mt-8">
+                  <div className="paragraph-block">
+                    <p className="mb-6 leading-relaxed">
+                    TailwindTemplates V2 is finally here! Our focus for TailwindTemplates v2 is to bring open-source design systems to Tailwindcss. Each design system has a detailed guideline to provide consistency and coherence within a user interface.</p>
+                  </div>
+                  <div className="header-brock">
+                    <h3 className="mb-6 font-bold">
+                    What to expect?</h3>
+                  </div>
+                  <div className="paragraph-block">
+                  <p className="mb-6 leading-relaxed">
+                    Over the coming months, we will expand the design systems that are currently available at launch. In addition to that, we are planning to create several tailwind configurations to easily get started with the proper colors and variants that belong to each design system. </p>
+                  </div>
+                  <div className="header-brock">
+                    <h3 className="mb-6 font-bold">
+                    Not only templates</h3>
+                  </div>
+                  <div className="paragraph-brock">
+                    <p className="mb-6 leagind-relaxed">
+                    We are planning to bring more tailwind and web development-related content such as videos, blog posts, tutorials and news. </p>
+                  </div>
+              </div>
+              
             </section>
 
 
-
-          </div>
+            </div>
           {/*フッダー*/}
           <div>
             <h1>About</h1>
